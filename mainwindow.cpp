88a30d9db1b073d73ebfc5208ebd2040cbd0c853
Dr Pepper <chibisaincrada@gmail.com>
2018-10-11 23:20:05 +0300

    got_it

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,69 +2,84 @@
 #include "ui_mainwindow.h"
 #include "perceptron.h"
 #include <QDebug>
-float x_in1[]={1,0,1,0},
-x_in2[]={0,1,0,1};
-float t1[]={0,1,1,0};
-float t2[]={0,1,1,1};
-void rescale(float*,float);
+float x_in1[]={0,1,0,1},
+x_in2[]={1,0,1,0},
+x_in3[]={1,1,1,1};
+
+float t1[]={0.1},
+t2[]={0.8},
+t3[]={1};
+
+vector<int> constr;
+
+void rescale(float*,int,float);
 perceptron* perc;
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    vector<int> constr;
+
     constr.push_back(4);
-    constr.push_back(3);
     constr.push_back(4);
+//    constr.push_back(3);
+    constr.push_back(1);
+
+    rand();
+//    rand();
+//    rand();
+
     perc=new perceptron(constr,1);
     for(int i=0;i<10000;i++)
     {
         perc->learn1(x_in1,t1);
         perc->learn1(x_in2,t2);
+        perc->learn1(x_in3,t3);
     }
 
 
     perc->refresh(x_in1);
-    qDebug()<<perc->lr[2]->n[2].state;
+    qDebug()<<perc->lr[perc->N-1]->n[0].state;
     perc->refresh(x_in2);
-    qDebug()<<perc->lr[2]->n[2].state;
-    perc->refresh(x_in1);
-    perc->showStates();
+    qDebug()<<perc->lr[perc->N-1]->n[0].state;
+    perc->refresh(x_in3);
+    qDebug()<<perc->lr[perc->N-1]->n[0].state;
 
     //rescale f
-    for(int i=0;i<3;i++)
-//        for(int j=0;j<4;j++)
+    for(int i=0;i<perc->N;i++)
             perc->lr[i]->n=new scaled_neuron[perc->lr[i]->size]();
-    rescale(x_in1,2);
-    rescale(x_in2,2);
-    rescale(t1,2);
-    rescale(t2,2);
+    rescale(x_in1,constr[0],2);
+    rescale(x_in2,constr[0],2);
+    rescale(x_in3,constr[0],2);
+    rescale(t1,1,2);
+    rescale(t2,1,2);
+    rescale(t3,1,2);
     perc->rescaleW(0.5);
 
-    perc->refresh(x_in1);
-    qDebug()<<perc->lr[2]->n[2].state;
-    perc->refresh(x_in2);
-    qDebug()<<perc->lr[2]->n[2].state;
+
 
     perc->refresh(x_in1);
     perc->showStates();
 
     perc->rescaleXShifts(20);
 
-    rescale(x_in1,20);
+    rescale(x_in1,constr[0],20);
     perc->refresh(x_in1);
     perc->showStates();
 
-    rescale(x_in2,20);
+    rescale(x_in2,constr[0],20);
     perc->refresh(x_in2);
+    perc->showStates();
+
+    rescale(x_in3,constr[0],20);
+    perc->refresh(x_in3);
     perc->showStates();
 }
 
 
-void rescale(float* a,float s)
+void rescale(float* a,int N,float s)
 {
-    for(int i=0;i<4;i++)
+    for(int i=0;i<N;i++)
         a[i]*=s;
 }
 
